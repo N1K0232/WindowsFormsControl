493ee7e1ffb86fc1323e73e02e210f56145e1829
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-01-29 18:26:23 +0100

    added WFTextBox.cs

---
+++ src/WindowsFormsControls/WFTextBox.Designer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsControls
+{
+    public partial class WFTextBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+        private TextBox textBox;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox.BorderStyle = BorderStyle.None;
+            textBox.Dock = DockStyle.Fill;
+            textBox.ForeColor = Color.Black;
+            textBox.Location = new Point(10, 7);
+            textBox.Name = "textBox";
+            textBox.Size = new Size(230, 22);
+            textBox.TabIndex = 0;
+            textBox.Click += new EventHandler(TextBox_Click);
+            textBox.TextChanged += new EventHandler(TextBox_TextChanged);
+            textBox.Enter += new EventHandler(TextBox_Enter);
+            textBox.KeyPress += new KeyPressEventHandler(TextBox_KeyPress);
+            textBox.Leave += new EventHandler(TextBox_Leave);
+            textBox.MouseEnter += new EventHandler(TextBox_MouseEnter);
+            textBox.MouseLeave += new EventHandler(TextBox_MouseLeave);
+            // 
+            // CustomTextBox
+            // 
+            this.AutoScaleMode = AutoScaleMode.None;
+            this.BackColor = SystemColors.Window;
+            this.Controls.Add(textBox);
+            this.Font = new Font("Segoe UI", 9.5F, FontStyle.Regular, GraphicsUnit.Point);
+            this.ForeColor = Color.DimGray;
+            this.Name = "WFTextBox";
+            this.Padding = new Padding(10, 7, 10, 7);
+            this.Size = new Size(250, 30);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ src/WindowsFormsControls/WFTextBox.cs
@@ -0,0 +1,559 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace WindowsFormsControls
+{
+    [DefaultEvent(nameof(TextChanged))]
+    public partial class WFTextBox : UserControl
+    {
+        private Color _borderColor;
+        private Color _borderFocusColor;
+        private int _borderSize;
+        private int _borderRadius;
+        private Color _placeHolderColor;
+        private string _placeHolderText;
+        private bool _isPlaceHolder;
+        private bool _isPasswordChar;
+        private bool _underlinedStyle;
+        private bool _isFocused;
+
+        public WFTextBox()
+        {
+            InitializeComponent();
+
+            _borderColor = Color.MediumSlateBlue;
+            _borderFocusColor = Color.HotPink;
+            _borderSize = 2;
+            _underlinedStyle = false;
+            _isFocused = false;
+            _borderRadius = 0;
+            _placeHolderColor = Color.Gray;
+            _placeHolderText = string.Empty;
+            _isPlaceHolder = false;
+            _isPasswordChar = false;
+        }
+
+        /// <summary>
+        /// gets or sets the border color of the textbox
+        /// </summary>
+        [Category("Control appearance")]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public Color BorderColor
+        {
+            get
+            {
+                return _borderColor;
+            }
+            set
+            {
+                _borderColor = value;
+                Invalidate();
+            }
+        }
+
+        /// <summary>
+        /// gets or sets the color of the border when the textbox is focused
+        /// </summary>
+        [Category("Control appearance")]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public Color BorderFocusColor
+        {
+            get
+            {
+                return _borderFocusColor;
+            }
+            set
+            {
+                _borderFocusColor = value;
+            }
+        }
+
+        /// <summary>
+        /// gets or sets the size of the border
+        /// </summary>
+        [Category("Control appearance")]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public int BorderSize
+        {
+            get
+            {
+                return _borderSize;
+            }
+            set
+            {
+                _borderSize = value;
+                Invalidate();
+            }
+        }
+
+        /// <summary>
+        /// gets or sets the style of the textbox
+        /// true if the control is underlined otherwise false
+        /// </summary>
+        [Category("Control appearance")]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public bool UnderlinedStyle
+        {
+            get
+            {
+                return _underlinedStyle;
+            }
+            set
+            {
+                _underlinedStyle = value;
+                Invalidate();
+            }
+        }
+
+        /// <summary>
+        /// gets or sets the radius of the border
+        /// </summary>
+        [Category("Control appearance")]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public int BorderRadius
+        {
+            get
+            {
+                return _borderRadius;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    _borderRadius = value;
+                    Invalidate();
+                }
+            }
+        }
+
+        /// <summary>
+        /// gets or sets the state of the textbox
+        /// true if the control uses the System Password char otherwise false
+        /// </summary>
+        [Category("Control appearance")]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public bool UseSystemPasswordChar
+        {
+            get
+            {
+                return _isPasswordChar;
+            }
+            set
+            {
+                _isPasswordChar = value;
+                textBox.UseSystemPasswordChar = value;
+            }
+        }
+
+        /// <summary>
+        /// gets or sets the password char of the textbox
+        /// </summary>
+        [Category("Control appearance")]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public char PasswordChar
+        {
+            get
+            {
+                return textBox.PasswordChar;
+            }
+            set
+            {
+                textBox.PasswordChar = value;
+            }
+        }
+
+        /// <summary>
+        /// gets or sets the status of the textbox
+        /// true if the textbox is multiline otherwise false
+        /// </summary>
+        [Category("Control appearance")]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public bool Multiline
+        {
+            get
+            {
+                return textBox.Multiline;
+            }
+            set
+            {
+                textBox.Multiline = value;
+            }
+        }
+
+        [Category("Control appearance")]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public override Color BackColor
+        {
+            get
+            {
+                return base.BackColor;
+            }
+            set
+            {
+                base.BackColor = value;
+                textBox.BackColor = value;
+            }
+        }
+
+        [Category("Control appearance")]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public override Color ForeColor
+        {
+            get
+            {
+                return base.ForeColor;
+            }
+            set
+            {
+                base.ForeColor = value;
+                textBox.ForeColor = value;
+            }
+        }
+
+        [Category("Control appearance")]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public override Font Font
+        {
+            get
+            {
+                return base.Font;
+            }
+            set
+            {
+                base.Font = value;
+                textBox.Font = value;
+                if (DesignMode)
+                {
+                    UpdateControlHeight();
+                }
+            }
+        }
+
+        [Category("Control appearance")]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public override string Text
+        {
+            get
+            {
+                return _isPlaceHolder ? string.Empty : textBox.Text;
+            }
+            set
+            {
+                textBox.Text = value;
+                SetPlaceHolder();
+            }
+        }
+
+        /// <summary>
+        /// gets or sets the color of the text of the placeholder
+        /// </summary>
+        [Category("Place holder")]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public Color PlaceHolderColor
+        {
+            get
+            {
+                return _placeHolderColor;
+            }
+            set
+            {
+                _placeHolderColor = value;
+                if (_isPlaceHolder)
+                {
+                    textBox.ForeColor = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// gets or sets the text of the placeholder
+        /// </summary>
+        [Category("Place holder")]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public string PlaceHolderText
+        {
+            get
+            {
+                return _placeHolderText;
+            }
+            set
+            {
+                _placeHolderText = value;
+                textBox.Text = "";
+                SetPlaceHolder();
+            }
+        }
+
+        public new event EventHandler TextChanged;
+
+        /// <summary>
+        /// redraws the control
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+            Control parent = Parent;
+            Rectangle clientRectangle = ClientRectangle;
+            int width = Width;
+            int height = Height;
+
+            if (_borderRadius > 1)
+            {
+                Rectangle rectBorderSmooth = clientRectangle;
+                Rectangle rectBorder = Rectangle.Inflate(rectBorderSmooth, -_borderSize, -_borderSize);
+                int smoothSize = _borderSize > 0 ? _borderSize : 1;
+
+                using var pathBorderSmooth = GetFigurePath(rectBorderSmooth, _borderRadius);
+                using var pathBorder = GetFigurePath(rectBorder, _borderRadius - _borderSize);
+                using var penBorderSmooth = new Pen(parent.BackColor, smoothSize);
+                using var penBorder = new Pen(_borderColor, _borderSize);
+
+                this.Region = new Region(pathBorderSmooth);
+                if (_borderRadius > 15)
+                {
+                    SetTextBoxRoundedRegion();
+                }
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                penBorder.Alignment = PenAlignment.Inset;
+
+                if (_isFocused)
+                {
+                    penBorder.Color = _borderFocusColor;
+                }
+
+                if (_underlinedStyle)
+                {
+                    g.DrawPath(penBorderSmooth, pathBorderSmooth);
+                    g.SmoothingMode = SmoothingMode.None;
+                    g.DrawLine(penBorder, 0, height - 1, width, height - 1);
+                }
+                else
+                {
+                    g.DrawPath(penBorderSmooth, pathBorderSmooth);
+                    g.DrawPath(penBorder, pathBorder);
+                }
+            }
+            else
+            {
+                using var penBorder = new Pen(_borderColor, _borderSize);
+                Region = new Region(clientRectangle);
+                penBorder.Alignment = PenAlignment.Inset;
+
+                if (_isFocused)
+                {
+                    penBorder.Color = _borderFocusColor;
+                }
+
+                if (_underlinedStyle)
+                {
+                    g.DrawLine(penBorder, 0, height - 1, width, height - 1);
+                }
+                else
+                {
+                    g.DrawLine(penBorder, 0, 0, width - 0.5F, height - 0.5F);
+                }
+            }
+        }
+
+        /// <summary>
+        /// loads the control
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            UpdateControlHeight();
+        }
+
+        /// <summary>
+        /// called when the control is resized
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if (DesignMode)
+            {
+                UpdateControlHeight();
+            }
+        }
+
+        /// <summary>
+        /// updates the height of the control 
+        /// when the text box is not in Multiline
+        /// </summary>
+        private void UpdateControlHeight()
+        {
+            if (!textBox.Multiline)
+            {
+                Font font = Font;
+                Size textSize = TextRenderer.MeasureText("Text", font);
+                int textHeight = textSize.Height + 1;
+                textBox.Multiline = true;
+                textBox.MinimumSize = new Size(0, textHeight);
+                textBox.Multiline = false;
+
+                Height = textBox.Height + Padding.Top + Padding.Bottom;
+            }
+        }
+
+        /// <summary>
+        /// sets the rounded region of the control
+        /// </summary>
+        private void SetTextBoxRoundedRegion()
+        {
+            GraphicsPath pathText;
+            Rectangle clientRectangle = textBox.ClientRectangle;
+
+            if (Multiline)
+            {
+                pathText = GetFigurePath(clientRectangle, _borderRadius - _borderSize);
+                textBox.Region = new Region(pathText);
+            }
+            else
+            {
+                pathText = GetFigurePath(clientRectangle, _borderSize * 2);
+                textBox.Region = new Region(pathText);
+            }
+        }
+
+        /// <summary>
+        /// sets the place holder
+        /// </summary>
+        private void SetPlaceHolder()
+        {
+            if (string.IsNullOrWhiteSpace(textBox.Text) && _placeHolderText != "")
+            {
+                _isPlaceHolder = true;
+                textBox.Text = _placeHolderText;
+                textBox.ForeColor = _placeHolderColor;
+
+                if (_isPasswordChar)
+                {
+                    textBox.UseSystemPasswordChar = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// removes the place holder
+        /// </summary>
+        private void RemovePlaceHolder()
+        {
+            if (_isPlaceHolder && _placeHolderText != "")
+            {
+                _isPlaceHolder = false;
+                textBox.Text = string.Empty;
+                textBox.ForeColor = ForeColor;
+                if (_isPasswordChar)
+                {
+                    textBox.UseSystemPasswordChar = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// called when the text of the textbox changes
+        /// </summary>
+        /// <param name="sender">the object that called the event</param>
+        /// <param name="e">the informations of the event</param>
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (TextChanged != null)
+            {
+                TextChanged.Invoke(sender, e);
+            }
+        }
+
+        /// <summary>
+        /// called when the textbox is clicked
+        /// </summary>
+        /// <param name="sender">the object that called the event</param>
+        /// <param name="e">the informations of the event</param>
+        private void TextBox_Click(object sender, EventArgs e)
+        {
+            OnClick(e);
+        }
+
+        /// <summary>
+        /// called when the mouse enters the textbox area
+        /// </summary>
+        /// <param name="sender">the object that called the event</param>
+        /// <param name="e">the informations of the event</param>
+        private void TextBox_MouseEnter(object sender, EventArgs e)
+        {
+            OnMouseEnter(e);
+        }
+
+        /// <summary>
+        /// called when the mouse leaves the textbox area
+        /// </summary>
+        /// <param name="sender">the object that called the event</param>
+        /// <param name="e">the informations of the event</param>
+        private void TextBox_MouseLeave(object sender, EventArgs e)
+        {
+            OnMouseLeave(e);
+        }
+
+        /// <summary>
+        /// called when a key is pressed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            OnKeyPress(e);
+        }
+
+        /// <summary>
+        /// called when the mouse enters the textbox area
+        /// </summary>
+        /// <param name="sender">the object that called the event</param>
+        /// <param name="e">the informations of the event</param>
+        private void TextBox_Enter(object sender, EventArgs e)
+        {
+            _isFocused = true;
+            Invalidate();
+            RemovePlaceHolder();
+        }
+
+        /// <summary>
+        /// called when the mouse leaves the textbox area
+        /// </summary>
+        /// <param name="sender">the object that called the event</param>
+        /// <param name="e">the informations of the event</param>
+        private void TextBox_Leave(object sender, EventArgs e)
+        {
+            _isFocused = false;
+            Invalidate();
+            SetPlaceHolder();
+        }
+
+        /// <summary>
+        /// gets the path of the control
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        private static GraphicsPath GetFigurePath(Rectangle rect, int radius)
+        {
+            GraphicsPath path = new();
+            float curveSize = radius * 2F;
+
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            path.AddArc(rect.Width - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Width - curveSize, rect.Height - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Height - curveSize, curveSize, curveSize, 90, 90);
+            path.CloseFigure();
+
+            return path;
+        }
+    }
+}
