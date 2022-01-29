3e5fbbe6c83d6a7a3d4360427455aa1046caab81
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-01-29 16:21:23 +0100

    added ToggleButton.cs

---
+++ src/WindowsFormsControls/ToggleButton.cs
@@ -0,0 +1,143 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace WindowsFormsControls
+{
+    public partial class ToggleButton : CheckBox
+    {
+        private Color _onBackColor;
+        private Color _offBackColor;
+        private Color _onToggleColor;
+        private Color _offToggleColor;
+
+        public ToggleButton()
+        {
+            _onBackColor = Color.MediumSlateBlue;
+            _onToggleColor = Color.WhiteSmoke;
+            _offBackColor = Color.Gray;
+            _offToggleColor = Color.Gainsboro;
+
+            MinimumSize = new Size(45, 22);
+        }
+
+        /// <summary>
+        /// gets or sets the background color when the button is on
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Category("Button appearance")]
+        public Color OnBackColor
+        {
+            get => _onBackColor;
+            set
+            {
+                _onBackColor = value;
+                Invalidate();
+            }
+        }
+
+        /// <summary>
+        /// gets or sets the background color when the button is off
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Category("Button appearance")]
+        public Color OffBackColor
+        {
+            get => _offBackColor;
+            set
+            {
+                _offBackColor = value;
+                Invalidate();
+            }
+        }
+
+        /// <summary>
+        /// gets or sets the toggle color when the button is on
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Category("Button appearance")]
+        public Color OnToggleColor
+        {
+            get => _onToggleColor;
+            set
+            {
+                _onToggleColor = value;
+                Invalidate();
+            }
+        }
+
+        /// <summary>
+        /// gets or sets the toggle color when the button is off
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Category("Button appearance")]
+        public Color OffToggleColor
+        {
+            get => _offToggleColor;
+            set
+            {
+                _offToggleColor = value;
+                Invalidate();
+            }
+        }
+
+        /// <summary>
+        /// redraws the control
+        /// </summary>
+        /// <param name="pe"></param>
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            int width = Width;
+            int height = Height;
+            int toggleSize = height - 5;
+            Control parent = Parent;
+            Graphics graphics = pe.Graphics;
+            SolidBrush backColor;
+            SolidBrush toggleColor;
+            GraphicsPath path = GetFigurePath();
+            Rectangle rectangle;
+
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            graphics.Clear(parent.BackColor);
+
+            if (Checked)
+            {
+                backColor = new SolidBrush(_onBackColor);
+                toggleColor = new SolidBrush(_onToggleColor);
+                rectangle = new Rectangle(width - height + 1, 2, toggleSize, toggleSize);
+
+                graphics.FillPath(backColor, path);
+                graphics.FillEllipse(toggleColor, rectangle);
+            }
+            else
+            {
+                backColor = new SolidBrush(_offBackColor);
+                toggleColor = new SolidBrush(_offToggleColor);
+                rectangle = new Rectangle(2, 2, toggleSize, toggleSize);
+
+                graphics.FillPath(backColor, path);
+                graphics.FillEllipse(toggleColor, rectangle);
+            }
+        }
+
+        /// <summary>
+        /// gets the path of the control
+        /// </summary>
+        /// <returns></returns>
+        private GraphicsPath GetFigurePath()
+        {
+            int arcSize = Height - 1;
+            Rectangle leftArc = new(0, 0, arcSize, arcSize);
+            Rectangle rightArc = new(Width - arcSize - 2, 0, arcSize, arcSize);
+
+            GraphicsPath path = new();
+            path.StartFigure();
+            path.AddArc(leftArc, 90, 180);
+            path.AddArc(rightArc, 270, 180);
+            path.CloseFigure();
+
+            return path;
+        }
+    }
+}
