8001e45b382ce5fc89767d5d7776ecf2349d5ee9
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-01-31 15:38:37 +0100

    added DropDownMenu.cs

---
+++ src/WindowsFormsControls/DropDownMenu.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using WindowsFormsControls.Helpers;
+
+namespace WindowsFormsControls
+{
+    public class DropDownMenu : ContextMenuStrip
+    {
+        private bool _isMainMenu;
+        private int _menuItemHeight = 25;
+        private Color _menuItemTextColor = Color.DimGray;
+        private Color _primaryColor = Color.MediumSlateBlue;
+
+        private Bitmap _menuItemHeaderSize;
+
+        public DropDownMenu(IContainer container)
+            : base(container)
+        {
+        }
+
+
+        [Browsable(false)]
+        public bool IsMainMenu
+        {
+            get
+            {
+                return _isMainMenu;
+            }
+            set
+            {
+                _isMainMenu = value;
+            }
+        }
+
+
+        [Browsable(false)]
+        public int MenuItemHeight
+        {
+            get
+            {
+                return _menuItemHeight;
+            }
+            set
+            {
+                _menuItemHeight = value;
+            }
+        }
+
+
+        [Browsable(false)]
+        public Color MenuItemTextColor
+        {
+            get
+            {
+                return _menuItemTextColor;
+            }
+            set
+            {
+                _menuItemTextColor = value;
+            }
+        }
+
+
+        [Browsable(false)]
+        public Color PrimaryColor
+        {
+            get
+            {
+                return _primaryColor;
+            }
+            set
+            {
+                _primaryColor = value;
+            }
+        }
+
+        private void LoadMenuItemAppearance()
+        {
+            if (_isMainMenu)
+            {
+                _menuItemHeaderSize = new Bitmap(25, 45);
+                _menuItemTextColor = Color.Gainsboro;
+            }
+            else
+            {
+                _menuItemHeaderSize = new Bitmap(15, _menuItemHeight);
+            }
+
+            foreach (ToolStripMenuItem menuItemL1 in Items)
+            {
+                menuItemL1.ForeColor = _menuItemTextColor;
+                menuItemL1.ImageScaling = ToolStripItemImageScaling.None;
+                if (menuItemL1.Image is null)
+                {
+                    menuItemL1.Image = _menuItemHeaderSize;
+                }
+
+                foreach (ToolStripMenuItem menuItemL2 in menuItemL1.DropDownItems)
+                {
+                    menuItemL2.ForeColor = _menuItemTextColor;
+                    menuItemL2.ImageScaling = ToolStripItemImageScaling.None;
+                    if (menuItemL2.Image is null)
+                    {
+                        menuItemL2.Image = _menuItemHeaderSize;
+                    }
+
+                    foreach (ToolStripMenuItem menuItemL3 in menuItemL2.DropDownItems)
+                    {
+                        menuItemL3.ForeColor = _menuItemTextColor;
+                        menuItemL3.ImageScaling = ToolStripItemImageScaling.None;
+                        if (menuItemL3.Image is null)
+                        {
+                            menuItemL3.Image = _menuItemHeaderSize;
+                        }
+
+                        foreach (ToolStripMenuItem menuItemL4 in menuItemL3.DropDownItems)
+                        {
+                            menuItemL4.ForeColor = _menuItemTextColor;
+                            menuItemL4.ImageScaling = ToolStripItemImageScaling.None;
+                            if (menuItemL4.Image is null)
+                            {
+                                menuItemL4.Image = _menuItemHeaderSize;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            if (!DesignMode)
+            {
+                LoadMenuItemAppearance();
+                Renderer = new MenuRenderer(_isMainMenu, _primaryColor, _menuItemTextColor);
+            }
+        }
+    }
+}
---
+++ src/WindowsFormsControls/Helpers/MenuRenderer.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace WindowsFormsControls.Helpers
+{
+    public class MenuRenderer : ToolStripProfessionalRenderer
+    {
+        private readonly Color primaryColor;
+        private readonly Color textColor;
+        private readonly int arrowThickness;
+
+        public MenuRenderer(bool isMainMenu, Color primaryColor, Color textColor)
+            : base(new MenuColorTable(isMainMenu, primaryColor))
+        {
+            this.primaryColor = primaryColor;
+            this.textColor = textColor;
+            arrowThickness = isMainMenu ? 3 : 2;
+        }
+
+        protected override void OnRenderItemText(ToolStripItemTextRenderEventArgs e)
+        {
+            base.OnRenderItemText(e);
+            e.Item.ForeColor = e.Item.Selected ? Color.White : textColor;
+        }
+        protected override void OnRenderArrow(ToolStripArrowRenderEventArgs e)
+        {
+            base.OnRenderArrow(e);
+            Graphics graphics = e.Graphics;
+            ToolStripItem item = e.Item;
+            Rectangle arrowRectangle = e.ArrowRectangle;
+            DrawArrow(graphics, item, arrowRectangle);
+        }
+
+        private void DrawArrow(Graphics graphics, ToolStripItem item, Rectangle arrowRectangle)
+        {
+            Size arrowSize = new(5, 12);
+            Color arrowColor = item.Selected ? Color.White : primaryColor;
+            Point location = arrowRectangle.Location;
+            Rectangle rectangle = new(location.X, (arrowRectangle.Height - arrowSize.Height) / 2,
+                arrowSize.Width, arrowSize.Height);
+
+            using var path = new GraphicsPath();
+            using var pen = new Pen(arrowColor, arrowThickness);
+
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            path.AddLine(rectangle.Left, rectangle.Top, rectangle.Right, rectangle.Top + rectangle.Height / 2);
+            path.AddLine(rectangle.Right, rectangle.Top + rectangle.Height / 2, rectangle.Left, rectangle.Top + rectangle.Height);
+            graphics.DrawPath(pen, path);
+        }
+    }
+}
