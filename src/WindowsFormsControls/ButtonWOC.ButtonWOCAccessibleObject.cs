69f7bd589050400465c04d1913b371b58ae4d191
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-02-05 12:00:37 +0100

    added ButtonWOCAccessibleObject class

---
+++ src/WindowsFormsControls/ButtonWOC.ButtonWOCAccessibleObject.cs
@@ -0,0 +1,13 @@
+﻿namespace WindowsFormsControls
+{
+    public partial class ButtonWOC
+    {
+        internal class ButtonWOCAccessibleObject : ButtonBaseAccessibleObject
+        {
+            public ButtonWOCAccessibleObject(ButtonWOC owner)
+                : base(owner)
+            {
+            }
+        }
+    }
+}
