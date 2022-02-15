e8e25d49ce5ac792c4a9122813ed6d8d3214e1a0
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-02-15 19:06:08 +0100

    added static fields and code refactoring

--- src/WindowsFormsControls/CuteButton.cs
+++ src/WindowsFormsControls/CuteButton.cs
@@ -10,9 +10,11 @@ namespace WindowsFormsControls
     /// </summary>
     public partial class CuteButton : Button, IButtonControl
     {
-        private Color _firstColor = Color.LightGreen;
-        private Color _secondColor = Color.DarkBlue;
+        private static readonly Color s_firstColor = Color.LightGreen;
+        private static readonly Color s_secondColor = Color.DarkBlue;
 
+        private Color _firstColor = Color.Empty;
+        private Color _secondColor = Color.Empty;
         private int _firstColorTransparency = 80;
         private int _secondColorTransparency = 80;
 
@@ -24,7 +26,7 @@ namespace WindowsFormsControls
         {
             Size = new Size(120, 50);
             ForeColor = Color.Black;
-            Font = new Font("Segoe UI", 12f);
+            Font = new Font("Segoe UI", 12F);
             FlatAppearance.BorderColor = Color.White;
             FlatAppearance.BorderSize = 0;
             FlatStyle = FlatStyle.Flat;
@@ -37,9 +39,17 @@ namespace WindowsFormsControls
         [Category("Button appearance")]
         public Color FirstColor
         {
-            get => _firstColor;
+            get
+            {
+                return GetFirstColor();
+            }
             set
             {
+                if (value == FirstColor)
+                {
+                    return;
+                }
+
                 _firstColor = value;
                 Invalidate();
             }
@@ -52,9 +62,17 @@ namespace WindowsFormsControls
         [Category("Button appearance")]
         public Color SecondColor
         {
-            get => _secondColor;
+            get
+            {
+                return GetSecondColor();
+            }
             set
             {
+                if (value == SecondColor)
+                {
+                    return;
+                }
+
                 _secondColor = value;
                 Invalidate();
             }
@@ -67,9 +85,17 @@ namespace WindowsFormsControls
         [Category("Button appearance")]
         public int FirstColorTransparency
         {
-            get => _firstColorTransparency;
+            get
+            {
+                return _firstColorTransparency;
+            }
             set
             {
+                if (value == FirstColorTransparency)
+                {
+                    return;
+                }
+
                 _firstColorTransparency = value;
                 Invalidate();
             }
@@ -82,9 +108,17 @@ namespace WindowsFormsControls
         [Category("Button appearance")]
         public int SecondColorTransparency
         {
-            get => _secondColorTransparency;
+            get
+            {
+                return _secondColorTransparency;
+            }
             set
             {
+                if (value == SecondColorTransparency)
+                {
+                    return;
+                }
+
                 _secondColorTransparency = value;
                 Invalidate();
             }
@@ -97,23 +131,71 @@ namespace WindowsFormsControls
         protected override void OnPaint(PaintEventArgs pe)
         {
             base.OnPaint(pe);
-            Graphics graphics = pe.Graphics;
-            graphics.SmoothingMode = SmoothingMode.AntiAlias;
-            DrawControl(graphics);
+            DrawControl(pe);
         }
 
         /// <summary>
         /// draws the control 
         /// </summary>
-        /// <param name="graphics"></param>
-        private void DrawControl(Graphics graphics)
+        /// <param name="pe">the event informations</param>
+        private void DrawControl(PaintEventArgs pe)
         {
-            Color c1 = Color.FromArgb(_firstColorTransparency, _firstColor);
-            Color c2 = Color.FromArgb(_secondColorTransparency, _secondColor);
+            Color firstColor = GetFirstColor();
+            Color secondColor = GetSecondColor();
+            Graphics graphics = pe.Graphics;
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            Draw(graphics, firstColor, secondColor);
+        }
+
+        /// <summary>
+        /// draws the control by giving the firstColor, the second color
+        /// and the graphics of the control
+        /// </summary>
+        /// <param name="graphics">the graphics of the control</param>
+        /// <param name="firstColor">the first color</param>
+        /// <param name="secondColor">the second color</param>
+        private void Draw(Graphics graphics, Color firstColor, Color secondColor)
+        {
+            Color c1 = Color.FromArgb(_firstColorTransparency, firstColor);
+            Color c2 = Color.FromArgb(_secondColorTransparency, secondColor);
             Rectangle rectangle = ClientRectangle;
             LinearGradientBrush brush = new(rectangle, c1, c2, 10);
             graphics.FillRectangle(brush, rectangle);
             brush.Dispose();
+        }
+
+        /// <summary>
+        /// gets the value of the _firstColor field
+        /// or the value of the s_firstColor field
+        /// </summary>
+        /// <returns>the firstColor of the control</returns>
+        private Color GetFirstColor()
+        {
+            Color c = _firstColor;
+
+            if (c.IsEmpty)
+            {
+                c = s_firstColor;
+            }
+
+            return c;
+        }
+
+        /// <summary>
+        /// gets the value of the _secondColor field
+        /// or the value of the s_secondColor field
+        /// </summary>
+        /// <returns>the second color of the control</returns>
+        private Color GetSecondColor()
+        {
+            Color c = _secondColor;
+
+            if (c.IsEmpty)
+            {
+                c = s_secondColor;
+            }
+
+            return c;
         }
     }
 }
